3a507c401c98bebac1b0449e0e1bc3d8bf454e92
hao wang <harrywang624@gmail.com>
2018-04-01 14:41:35 -0400

    abt is pass most tests besides the buffer problems, add queue.c to test if can work

--- cse489589_assignment2/hwang67/src/abt.c
+++ cse489589_assignment2/hwang67/src/abt.c
@@ -11,7 +11,7 @@
    protocols (from A to B). Network properties:
    - one way network delay averages five time units (longer if there
      are other messages in the channel for GBN), but can be larger
-   - packets can be corrupted (either the header or the data portion)
+- packets can be corrupted (either the header or the data portion)
      or lost, according to user-defined probabilities
    - packets will be delivered in the order in which they were sent
      (although some can be lost).
@@ -19,19 +19,21 @@
 
 /********* STUDENTS WRITE THE NEXT SIX ROUTINES *********/
 /*0 (for A-side delivery) or 1 (for B-side delivery) */
-#define TIMEOUT 10.0
+#define TIMEOUT 15.0
 char senBuf[512]; //buffer for application layer sending msg
 char revBuf[512]; //buffer for application layer receving msg
 struct pkt tmpPkt;
+
 int seqnum;
 int acknum;
+int lastchecksum;
 
 int calculate_checksum(packet)
 struct pkt packet;
 {
   int checksum = 0;
   checksum += packet.seqnum;
-  checksum += packet.acknum;
+  // checksum += packet.acknum;
   for(int i = 0; i < sizeof(packet.payload) / sizeof(char); i++){
     checksum += packet.payload[i];
   }
@@ -50,33 +52,35 @@ struct pkt packet;
 void A_output(message)
   struct msg message;
 {
-  // printf("run A_output\n");
+  printf("run A_output\n");
   struct pkt sendingpkt;
 
   strncpy(sendingpkt.payload, message.data, 20);
   sendingpkt.seqnum = seqnum;
   sendingpkt.checksum = calculate_checksum(sendingpkt);
+  printf("A sending : %s, seq: %d\n", sendingpkt.payload, sendingpkt.seqnum);
 
   //save the sending pkt to tmp incase need to retransmission
+  // tmpPkt = &sendingpkt;
   strncpy(tmpPkt.payload, sendingpkt.payload, 20);
   tmpPkt.seqnum = sendingpkt.seqnum;
-  tmpPkt.acknum = sendingpkt.acknum;
+  // tmpPkt.acknum = sendingpkt.acknum;
   tmpPkt.checksum = sendingpkt.checksum;
 
   tolayer3(0, sendingpkt);
   starttimer(0, TIMEOUT);
-
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(packet)
   struct pkt packet;
 {
-  // printf("run A_input\n");
-  if (packet.acknum == acknum){
-    acknum = packet.acknum == 0 ? 1: 0;
+  printf("run A_input\n");
+  printf("A receving ack: %d, seqnum is: %d\n", packet.acknum, seqnum);
 
-    seqnum = packet.seqnum == 0 ? 1: 0;
+  if (packet.acknum == seqnum){
+
+    seqnum = packet.acknum == 0 ? 1: 0;
     stoptimer(0);
   }
 }
@@ -85,18 +89,21 @@ void A_input(packet)
 void A_timerinterrupt()
 {
   printf("run A_timerinterrupt\n");
-  tolayer3(0, tmp);
-  starttimer();
+  printf("A resending : %s, seq: %d\n", tmpPkt.payload, tmpPkt.seqnum);
+
+  tolayer3(0, tmpPkt);
+  starttimer(0, TIMEOUT);
+
 }
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
-  // printf("run A_init\n");
+  printf("run A_init\n");
   // memest(&tmp,0 ,sizeof(struct pkt));
   seqnum = 0;
-  acknum = 0;
+  // acknum = 0;
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -104,26 +111,31 @@ void A_init()
 void B_input(packet)
   struct pkt packet;
 {
-  //deplicate pkt
-  if (packet.seqnum != seqnum){
-    memset(&packet.payload[0], 0, sizeof(packet.payload));
-    packet.acknum = packet.seqnum;
-    //send ack packet
-    tolayer3(1, packet);
-  }
+  struct pkt ackPkt;
+  printf("run B_input\n");
+  printf("B receving: %20s, seqnum: %d, current acknum: %d\n", packet.payload, packet.seqnum, acknum);
 
   //compare checksum
   int isCheckSumVaild = vaildiate_checksum(packet);
-  if (isCheckSumVaild){
-    // printf("run B_input\n");
-    //receving packet
-    tolayer5(1, packet.payload);
 
-    memset(&packet.payload[0], 0, sizeof(packet.payload));
-    packet.acknum = packet.seqnum;
-    //send ack packet
-    tolayer3(1, packet);
-    seqnum = seqnum == 0 ? 1 : 0;
+  if (isCheckSumVaild){
+    //receving packet
+    //check if deplicate pkt
+    if (packet.seqnum == acknum){
+      tolayer5(1, packet.payload);
+      //send ack packet
+      ackPkt.acknum = packet.seqnum;
+      printf("B sending ack: %d\n", ackPkt.acknum);
+      tolayer3(1, ackPkt);
+      acknum = acknum == 0 ? 1 : 0;
+    }else{
+      //duplicate packet
+      ackPkt.acknum = packet.seqnum;
+      printf("B sending ack: %d\n", ackPkt.acknum);
+      tolayer3(1, ackPkt);
+      // acknum = acknum == 0 ? 1 : 0;
+    }
+
   }
 }
 
@@ -131,8 +143,6 @@ void B_input(packet)
 /* entity B routines are called-> You can use it to do any initialization */
 void B_init()
 {
-  // printf("run B_init\n");
-  seqnum = 0;
+  printf("run B_init\n");
   acknum = 0;
-
 }
--- cse489589_assignment2/hwang67/src/gbn.c
+++ cse489589_assignment2/hwang67/src/gbn.c
@@ -20,7 +20,10 @@
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
 /* called from layer 5, passed the data to be sent to other side */
+int base;
 int win_size;
+int seqnum;
+int acknum;
 
 int calculate_checksum(packet)
 struct pkt packet;
@@ -44,22 +47,34 @@ struct pkt packet;
 void A_output(message)
   struct msg message;
 {
-  printf("run A_output\n");
+  // printf("run A_output\n");
+  // printf("run A_output\n");
+  struct pkt sendingpkt;
 
+  strncpy(sendingpkt.payload, message.data, 20);
+  sendingpkt.seqnum = seqnum;
+  sendingpkt.checksum = calculate_checksum(sendingpkt);
+
+  tolayer3(0, sendingpkt);
+  starttimer(0, TIMEOUT);
+  seqnum += 1;
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(packet)
   struct pkt packet;
 {
-  printf("run A_input\n");
-
+  // printf("run A_input\n");
+  if (packet.acknum == seqnum - win_size){
+      seqnum += 1;
+      stoptimer(0);
+  }
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-  printf("run A_timerinterrupt\n");
+  // printf("run A_timerinterrupt\n");
 
 }
 
@@ -67,8 +82,10 @@ void A_timerinterrupt()
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
-  printf("run A_init\n");
+  // printf("run A_init\n");
+  base = 0;
   win_size = getwinsize();
+  seqnum = 0;
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -77,14 +94,35 @@ void A_init()
 void B_input(packet)
   struct pkt packet;
 {
-  printf("run B_input\n");
+  // printf("run B_input\n");
+  //deplicate pkt
+  if (packet.seqnum != acknum){
+    memset(&packet.payload[0], 0, sizeof(packet.payload));
+    packet.acknum = acknum;
+    //send ack packet
+    tolayer3(1, packet);
+  }
 
+  //compare checksum
+  int isCheckSumVaild = vaildiate_checksum(packet);
+  if (isCheckSumVaild){
+    // printf("run B_input\n");
+    //receving packet
+    tolayer5(1, packet.payload);
+
+    memset(&packet.payload[0], 0, sizeof(packet.payload));
+    packet.acknum = packet.seqnum;
+    //send ack packet
+    tolayer3(1, packet);
+    acknum += 1;
+  }
 }
 
 /* the following rouytine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
-  printf("run B_init\n");
-
+  // printf("run B_init\n");
+  win_size = getwinsize();
+  acknum = 0;
 }
